6495dab2c43b5503fa4196e9c8a05ecaa7089add
Premu-ahir <premahir610@gmail.com>
2024-08-09 17:06:05 +0530

    Add files via upload

---
+++ 30.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    float DAYS_IN_YEAR = 365.25;
+    float years, days;
+    float days_from_years;
+    float years_from_days;
+
+    printf("Enter number of years: ");
+    scanf("%f", &years);
+    days_from_years = years * DAYS_IN_YEAR;
+    printf("%.2f years is approximately %.2f days.\n", years, days_from_years);
+
+    printf("Enter number of days: ");
+    scanf("%f", &days);
+    years_from_days = days / DAYS_IN_YEAR;
+    printf("%.2f days is approximately %.2f years.\n", days, years_from_days);
+
+ 
+}
